bff62f0655de7599a8640a5faf5e39a00d571d9e
songzxDev <songzx_2326@163.com>
2019-09-08 16:39:45 +0800

    参照老师代码，初步仿照实现tab组件

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-import {Carousel} from "./component.js";
+import Tab from "./tabs.js";
+import Div from "./container.js";
 
-function myCreate(Class, attributes){
+function myCreate(Class, attributes, ...children) {
     let object = new Class();
-    for(let name in attributes)
+    for (let name in attributes) {
         object.setAttribute(name, attributes[name]);
+    }
+    for (let child of children) {
+        object.appendChild(child);
+    }
+
     return object;
 }
 
 
-let c = <Carousel width="100"></Carousel>;
+let c = <Tab style="width: 100%; height:400px;">
+    <Div tab-title="推荐" style="background-color:red;"></Div>
+    <Div tab-title="有趣的店" style="background-color:green;"></Div>
+    <Div tab-title="品牌新店" style="background-color:yellow;"></Div>
+</Tab>;
 c.appendTo(document.body);
