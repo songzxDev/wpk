dda95f75a9d00c41a06f0c15154b2fbe950326fa
songzxDev <songzx_2326@163.com>
2019-11-05 21:20:59 +0800

    视觉还原第一个页面收藏店铺展示

--- script.js
+++ script.js
@@ -31,6 +31,49 @@ window.render = function (data, root) {
                 style="margin: 28px; height: 492px; width: 924px;; overflow: hidden; white-space: nowrap;"
                 pictures={pictures}>
             </Carousel>
+            <div style="margin: 69px 34px 40px; height: 43px; font-family: PingFangSC-Medium; font-size: 46px; font-weight: normal; font-stretch: normal; line-height: 35px; letter-spacing: 0px; color: #333333;">
+                超多人气收藏的店
+            </div>
+            <div style="margin: 0 34px 35px; display: flex;">
+                <div style="height: 361px; width: 50%; margin-right: 25px; border-radius: 18px; background-color: #fefefe;">
+                    <div style="margin: 27px 35px; display: flex;">
+                        <div style="width: 78px; height: 78px;"></div>
+                        <div style=" height: 78px; display: flex; flex-flow: column;">
+                            <div style="margin-left: 18px; height: 34px; font-family: PingFangSC-Regular; font-size: 36px; font-weight: normal; font-stretch: normal; line-height: 35px; letter-spacing: 0px; color: #333333;">
+                                极客时间旗舰店
+                            </div>
+                            <div style="margin:18px 0 0 18px; width: 63px; height: 30px; font-family: PingFangSC-Regular; font-size: 26px; line-height: 25px; background-color: #ee0507; border-radius: 15px; color: #FFFFFF;">天猫</div>
+                        </div>
+                    </div>
+                    <div style="margin: 0 35px 30px 35px; height: 199px; display: flex;">
+                        <div style="border-radius: 18px; height: 100%; width: 199px; margin-right: 28px;">
+                            <img style="border-radius: 18px; width:100%; height:100%" src="https://img.alicdn.com/imgextra/i1/3954877919/O1CN01pQ18Jj28MwxyqWBXe_!!3954877919.jpg_430x430q90.jpg" alt=""/>
+                        </div>
+                        <div style="border-radius: 18px; height: 100%; width: 199px;">
+                            <img style="border-radius: 18px; width:100%; height:100%" src="https://img.alicdn.com/imgextra/i3/2191160435/TB19WqyqASWBuNjSszdXXbeSpXa_!!0-item_pic.jpg_430x430q90.jpg" alt=""/>
+                        </div>
+                    </div>
+                </div>
+                <div style="height: 361px; width: 50%; border-radius: 18px; background-color: #fefefe;">
+                    <div style="margin: 27px 35px; display: flex;">
+                        <div style="width: 78px; height: 78px;"></div>
+                        <div style=" height: 78px; display: flex; flex-flow: column;">
+                            <div style="margin-left: 18px; height: 34px; font-family: PingFangSC-Regular; font-size: 36px; font-weight: normal; font-stretch: normal; line-height: 35px; letter-spacing: 0px; color: #333333;">
+                                当当旗舰店
+                            </div>
+                            <div style="margin:18px 0 0 18px; width: 63px; height: 30px; font-family: PingFangSC-Regular; font-size: 26px; line-height: 25px; background-color: #ee0507; border-radius: 15px; color: #FFFFFF;">天猫</div>
+                        </div>
+                    </div>
+                    <div style="margin: 0 35px 30px 35px; height: 199px; display: flex;">
+                        <div style="border-radius: 18px; height: 100%; width: 199px; margin-right: 28px;">
+                            <img style="border-radius: 18px; width:100%; height:100%" src="https://img.alicdn.com/imgextra/i1/2406931838/O1CN01WjXfUV1PRqWaM5Au0_!!2406931838.jpg_1152x1920Q50s50.jpg" alt=""/>
+                        </div>
+                        <div style="border-radius: 18px; height: 100%; width: 199px;">
+                            <img style="border-radius: 18px; width:100%; height:100%" src="https://img.alicdn.com/imgextra/i4/2274338009/O1CN01vC8AkK292AdnMTgq5_!!2274338009.jpg_430x430q90.jpg" alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <ListView data={data}>
             </ListView>
         </ScrollView>
